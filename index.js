e7f7c3ba5bb49629a02cbb7c1a8e78ebacb8b637
DavidStephenss <dvdstephensd@gmail.com>
2020-07-28 21:09:28 -0500

    just another push

--- index.js
+++ index.js
@@ -17,3 +17,48 @@ const getPrompt = () => {
     })
   })
 }
+
+class BankAccount {
+  constructor(accountnumber, owner){
+  this.accountnumber = accountnumber
+  this.owner = owner
+  this.transaction = []
+  }
+  balance() { 
+       function sumTotal(total, num) {    
+         return total + num     
+         }
+             return transactions.reduce(sumTotal) 
+             }
+
+  }
+  deposit(amt) {
+    this.transaction = amt
+    amt.push(this.transaction)
+  }
+  charge(payee, amt) {
+    parseInt(this.transaction)
+  }
+}
+
+
+class Transaction {
+  constructor (amount, payee){
+    this.date = date
+    this.amount = amount
+    this.payee = payee
+  }
+}
+ const bankAccount1 = new BankAccount('900-300-200', 'bob dylan') 
+ console.log(bankAccount1.transaction.length)
+if (typeof describe === 'function') {
+  describe('BankAccount', function(){
+    it('should be a number', function(){
+      const bankAccount1 = new BankAccount('900-300-200', 'bob dylan') 
+      assert.equal(bankAccount1.accountnumber, '900-300-200')
+      assert.equal(bankAccount1.name,'bob dylan')
+      assert.equal(bankAccount1.transaction.length, 0 )
+
+    })
+  })  
+}
